fe1ac04982e8571fbac1b728733097d664f54b91
bshakir <burhanshakir96@gmail.com>
2017-10-04 09:46:17 +0100

    First Commit

---
+++ Assets/scripts/carController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+	public float carSpeed;
+	Vector3 position;
+	public audioManager am;
+
+	bool currentPlatformForAndroid = false;
+
+	public uiManager ui;
+	// Use this for initialization
+
+	Rigidbody2D rb;
+
+	void Awake(){
+		rb = GetComponent<Rigidbody2D> ();
+
+		#if UNITY_ANDROID
+		currentPlatformForAndroid = true;
+		#endif
+
+	}
+
+
+	void Start () {
+
+		position = transform.position;
+		am.carSound.Play();
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentPlatformForAndroid) {
+
+			accelarometerMove ();
+
+		} else {
+
+			position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+
+			}
+
+		position = transform.position;
+		position.x = Mathf.Clamp (position.x, -2.0f, 2.0f);
+
+		transform.position = position;
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+
+		if (col.gameObject.tag == "Enemy Car") {
+			//Destroy (gameObject);
+			gameObject.SetActive(false);
+			ui.gameOver ();
+			am.carSound.Stop ();
+		}
+	}
+
+	void accelarometerMove(){
+		float x = Input.acceleration.x;
+
+		Debug.Log ("X = " + x);
+
+
+		if (x < -0.15f) {
+			MoveLeft ();
+		} else if (x > 0.15f) {
+			MoveRight ();	
+		} 
+		else {
+			SetVelocityZero();
+		}
+	
+	}
+
+	public void MoveLeft(){
+		rb.velocity = new Vector2 (-carSpeed, 0);
+	}
+
+	public void MoveRight(){
+
+		rb.velocity = new Vector2 (carSpeed, 0);
+	}
+
+	public void SetVelocityZero(){
+		rb.velocity = Vector2.zero;
+	}
+}
---
+++ Assets/scripts/uiManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class uiManager : MonoBehaviour {
+
+	public Button[] buttons;
+	public Text scoreText;
+	int score;
+	bool isGameOver;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+
+		isGameOver = false;
+		InvokeRepeating ("scoreUpdate", 1.0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = "Score : " + score;
+	}
+
+	public void Play()
+	{
+		Time.timeScale = 1;
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("level1");
+	}
+
+
+	public void Pause()
+	{
+		if (Time.timeScale == 1) {
+			Time.timeScale = 0;
+		} 
+		else if (Time.timeScale == 0) {
+			Time.timeScale = 1;
+		}
+	}
+
+	void scoreUpdate(){
+
+		if (!isGameOver) {
+
+			score += 1;
+
+		}
+	}
+
+	public void gameOver()
+	{
+		isGameOver = true;
+		Time.timeScale = 0;
+		foreach (Button button in buttons) {
+		
+			button.gameObject.SetActive(true);
+		}
+
+	}
+
+	public void Menu()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("menuScene");
+	}
+
+	public void Exit()
+	{
+		Application.Quit ();
+	}
+
+
+
+}
